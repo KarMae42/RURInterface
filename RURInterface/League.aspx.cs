14276070ff8713f88dbdc07ceb00f1e09edae1bf
Kara Caron Caron <kara.caron@365.elmhurst.edu>
2019-05-07 08:32:36 -0500

    Work on the navigation bar

--- RURInterface/League.aspx.cs
+++ RURInterface/League.aspx.cs
@@ -5,13 +5,17 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Data;
+using Microsoft.AspNet.FriendlyUrls;
+
 namespace RURInterface
 {
     public partial class League : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Master.HeaderText = "League Page";
+            //Master.HeaderText = "League Page";
+            
         }
     }
 }
--- RURInterface/Site.Master.designer.cs
+++ RURInterface/Site.Master.designer.cs
@@ -31,15 +31,6 @@ namespace RURInterface {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// blNav control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.BulletedList blNav;
-        
-        /// <summary>
         /// lblPageHeader control.
         /// </summary>
         /// <remarks>
