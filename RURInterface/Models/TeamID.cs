87679efcf969e96e4b0b14ceb02563af7a1192b5
Kara Caron Caron <kara.caron@365.elmhurst.edu>
2019-05-08 00:05:47 -0500

    TeamID.cs created

---
+++ RURInterface/Models/TeamID.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class TeamID
+{
+    public string TeamName { get; set; }
+    public string Mascot { get; set; }
+    public string Region { get; set; }
+    public string Rank { get; set; }
+    public string Record { get; set; }
+    public string Logo { get; set; }
+}
