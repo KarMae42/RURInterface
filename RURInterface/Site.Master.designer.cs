6d77836bc203fe8efd3fb716d867e892b7e7e531
mollyBrennan <31749149+mollyBrennan@users.noreply.github.com>
2019-05-22 00:01:16 -0500

    readded form

--- RURInterface/Site.Master.designer.cs
+++ RURInterface/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace RURInterface {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder headPlaceHolder;
         
         /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
         /// lblPageHeader control.
         /// </summary>
         /// <remarks>
